bb357d6d95ffbbd27f60abf8b17bf7f9ecd3c280
YoKo28 <y.koleva@student.rug.nl>
2024-10-21 14:39:42 +0200

    fixing paths for saving tokenized datasets and updating the requirnments to numpy

--- src/data_processing/loading_tokenization.py
+++ src/data_processing/loading_tokenization.py
@@ -20,8 +20,8 @@ def establish_data_path():
     path = os.getcwd()
 
     # If you want to run the file specifically from this file, uncomment the two lines below
-    #path = os.path.abspath(os.path.join(path, os.pardir))
-    #path = os.path.abspath(os.path.join(path, os.pardir))
+    path = os.path.abspath(os.path.join(path, os.pardir))
+    path = os.path.abspath(os.path.join(path, os.pardir))
     path = os.path.join(path, 'data/raw/GYAFC_Corpus')
     print(path)
     return path
@@ -55,7 +55,7 @@ def check_gpu_availability():
 
 
 # Adding prefix tokens for the formal and informal datasets
-def tokenizing(tokenizer, data, split):
+def tokenizing(tokenizer, data, split,theme):
     tokenizer.add_tokens(['<|formal|>', '<|informal|>'])
 
     # Add prefixes to all input sentences and targets to indicate the style of the given sentence
@@ -81,9 +81,19 @@ def tokenizing(tokenizer, data, split):
     # Converting the processed data into an instance of the Dataset class to prepare for training
     dataset_processed = FormalityTransferDataset(data_processed)
 
+    # Getting the current directory and move up two levels to reach the project root
+    project_root = os.path.abspath(os.path.join(os.getcwd(), '..', '..'))
+
+    # Appending the correct data path
+    save_path = os.path.join(project_root, 'data', 'processed')
+
+    # Checking if the directory exists, and if not, create it
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
     # Pickling the tokenized dataset to avoid pre-processing multiple times
-    with open(f'{split}_dataset_processed.pkl', 'wb') as f:
+    with open(os.path.join(save_path, f'{theme}_{split}_dataset_processed.pkl'), 'wb') as f:
         pickle.dump(dataset_processed, f)
+
 
     return dataset_processed
 
@@ -100,9 +110,9 @@ def main():
                                               pad_token='<|pad|>')
 
     # Tokenizing the train, tune and test splits
-    tokenized_train = tokenizing(tokenizer, train, "train")
-    tokenized_tune = tokenizing(tokenizer, tune, "tune")
-    tokenized_test = tokenizing(tokenizer, test, "test")
+    tokenized_train_ent = tokenizing(tokenizer, train, "train","entertainment")
+    tokenized_tune_ent = tokenizing(tokenizer, tune, "tune", "entertainment")
+    tokenized_test_ent = tokenizing(tokenizer, test, "test","entertainment")
 
 
 if __name__ == "__main__":
