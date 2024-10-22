3d9e2f588896b65d6d8d293cce48247a680a71b5
YoKo28 <y.koleva@student.rug.nl>
2024-10-22 20:28:53 +0200

    Joining the sequences in the appropriate format, changing the Dataset class a bit

--- src/data_processing/loading_tokenization.py
+++ src/data_processing/loading_tokenization.py
@@ -9,8 +9,8 @@ import pickle
 # Defining constants
 # Parameters
 MODEL_CHECKPOINT = 'gpt2-medium'
-PREFIX_FORMAL = '<|formal|>'
-PREFIX_INFORMAL = '<|informal|>'
+PREFIX_FORMAL = '[Formal]'
+PREFIX_INFORMAL = '[Informal]'
 MAX_INPUT_LEN = 2048
 MAX_TARGET_LEN = 128
 
@@ -20,8 +20,8 @@ def establish_data_path():
     path = os.getcwd()
 
     # If you want to run the file specifically from this file, uncomment the two lines below
-    #path = os.path.abspath(os.path.join(path, os.pardir))
-    #path = os.path.abspath(os.path.join(path, os.pardir))
+    # path = os.path.abspath(os.path.join(path, os.pardir))
+    # path = os.path.abspath(os.path.join(path, os.pardir))
     path = os.path.join(path, 'data/raw/GYAFC_Corpus')
     print(path)
     return path
@@ -33,17 +33,17 @@ def load_train_tune_test(theme, path):
     formal_train = pd.read_table(path + f'/{theme}/train/formal', names=['label'], header=None, quoting=csv.QUOTE_NONE)
     informal_train = pd.read_table(path + f'/{theme}/train/informal', names=['input_sentence'], header=None,
                                    quoting=csv.QUOTE_NONE)
-    train = pd.concat([informal_train, formal_train], axis=1)
+    train = pd.DataFrame(PREFIX_INFORMAL + informal_train['input_sentence'] + PREFIX_FORMAL + formal_train['label'])
 
     formal_tune = pd.read_table(path + f'/{theme}/tune/formal', names=['label'], header=None, quoting=csv.QUOTE_NONE)
     informal_tune = pd.read_table(path + f'/{theme}/tune/informal.ref0', names=['input_sentence'], header=None,
                                   quoting=csv.QUOTE_NONE)
-    tune = pd.concat([informal_tune, formal_tune], axis=1)
+    tune = pd.DataFrame(PREFIX_INFORMAL + informal_tune['input_sentence'] + PREFIX_FORMAL + formal_tune['label'])
 
     formal_test = pd.read_table(path + f'/{theme}/test/formal', names=['label'], header=None, quoting=csv.QUOTE_NONE)
     informal_test = pd.read_table(path + f'/{theme}/test/informal.ref0', names=['input_sentence'], header=None,
                                   quoting=csv.QUOTE_NONE)
-    test = pd.concat([informal_test, formal_test], axis=1)
+    test = pd.DataFrame(PREFIX_INFORMAL + informal_test['input_sentence'] + PREFIX_FORMAL + formal_test['label'])
 
     return train, tune, test
 
@@ -55,28 +55,22 @@ def check_gpu_availability():
 
 
 # Adding prefix tokens for the formal and informal datasets
-def tokenizing(tokenizer, data, split,theme):
-    tokenizer.add_tokens(['<|formal|>', '<|informal|>'])
+def tokenizing(tokenizer, data, split, theme):
+    # Adding the tokens to the model, so it knows to treat them as special tokens
+    tokenizer.add_tokens([PREFIX_FORMAL, PREFIX_INFORMAL])
 
-    # Add prefixes to all input sentences and targets to indicate the style of the given sentence
-    data['input_sentence'] = data['input_sentence'].apply(lambda x: PREFIX_INFORMAL + x)
-    data['label'] = data['label'].apply(lambda x: PREFIX_FORMAL + x)
 
-    # tokenization of the input sequences and the labels
-    model_inputs = tokenizer(data['input_sentence'].tolist(), max_length=MAX_INPUT_LEN, truncation=True, padding=True,
-                             return_tensors='pt')
-    targets = tokenizer(data['label'].tolist(), max_length=MAX_INPUT_LEN, truncation=True, padding=True,
-                        return_tensors='pt')
+    # tokenization of the conjoined input formal sequences and their formal targets
+    formal_informal_conjoined = tokenizer(data.iloc[:, 0].tolist(), max_length=MAX_INPUT_LEN, truncation=True,
+                                          padding=True,
+                                          return_tensors='pt')
 
     # Converting the processed data into a dictionary
     data_processed = {
-        'input_ids': model_inputs['input_ids'],
-        'attention_mask': model_inputs['attention_mask'],
-        'labels': targets['input_ids'],
-        'decoder_attention_mask': targets['attention_mask'],
-    }
+        'input_ids': formal_informal_conjoined['input_ids'],
+        'attention_mask': formal_informal_conjoined['attention_mask']
 
-    print(len(model_inputs['input_ids']), len(targets['input_ids']))
+    }
 
     # Converting the processed data into an instance of the Dataset class to prepare for training
     dataset_processed = FormalityTransferDataset(data_processed)
@@ -94,7 +88,6 @@ def tokenizing(tokenizer, data, split,theme):
     with open(os.path.join(save_path, f'{theme}_{split}_dataset_processed.pkl'), 'wb') as f:
         pickle.dump(dataset_processed, f)
 
-
     return dataset_processed
 
 
@@ -110,9 +103,9 @@ def main():
                                               pad_token='<|pad|>')
 
     # Tokenizing the train, tune and test splits
-    tokenized_train_ent = tokenizing(tokenizer, train, "train","entertainment")
+    tokenized_train_ent = tokenizing(tokenizer, train, "train", "entertainment")
     tokenized_tune_ent = tokenizing(tokenizer, tune, "tune", "entertainment")
-    tokenized_test_ent = tokenizing(tokenizer, test, "test","entertainment")
+    tokenized_test_ent = tokenizing(tokenizer, test, "test", "entertainment")
 
 
 if __name__ == "__main__":
