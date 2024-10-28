a7ebca2865b1191d180c6865832b45ff98fc6d23
Jules Roboz <r.julilla@gmail.com>
2024-10-28 16:09:46 +0100

    cleaned up code (removed unnecessary comments)

--- src/models/streamlit.py
+++ src/models/streamlit.py
@@ -11,22 +11,17 @@ from fastapi import UploadFile, HTTPException
 
 st.title('Formality Transfer')
 
-# Load the tokenizer and model
-
+# Load the non-peft tokenizer and model
 my_model = AutoModelForCausalLM.from_pretrained('full_FT_model_changedtokens', use_safetensors=True)
 my_tokenizer = GPT2Tokenizer.from_pretrained('full_FT_model_changedtokens')
-#my_tokenizer.pad_token = '[PAD]'  # Assuming you have defined a padding token
-#my_model.config.pad_token_id = my_tokenizer.pad_token_id
 
+#define paths for lora model and tokenizer
 tokenizer_path = 'trained_tokenizer'
 model_path = 'base_model'
-test_path = 'data/processed/entertainment_test_dataset_processed.pkl'
 lora_model_path = 'lora_trained'
 
-# Load tokenizer from its directory
-lora_tokenizer = GPT2Tokenizer.from_pretrained(tokenizer_path)
-
 # Load the saved LoRA configuration
+lora_tokenizer = GPT2Tokenizer.from_pretrained(tokenizer_path)
 config = PeftConfig.from_pretrained(lora_model_path)
 base_model = AutoModelForCausalLM.from_pretrained(model_path, device_map={"": "cpu"})
 base_model.resize_token_embeddings(len(lora_tokenizer))
@@ -39,7 +34,7 @@ lora_model.eval()
 prompt = st.chat_input("Please input a short informal text, and I will respond with the formal version!")
 if prompt:
     try:
-        # Preprocess the text, include attention_mask
+        # Preprocess the text for both models, include attention_mask
         inputs = my_tokenizer(prompt, padding=True, truncation=True, return_tensors="pt")
         input_ids = inputs['input_ids']
         attention_mask = inputs['attention_mask']
@@ -52,16 +47,14 @@ if prompt:
         raise HTTPException(status_code=400, detail=f"Error in text preprocessing: {str(e)}")
 
     input_text = prompt
-    # Generate output from the model using both input_ids and attention_mask
+    # Generate output from the non-peft model using both input_ids and attention_mask
     outputs_base = my_model.generate(input_ids=input_ids, attention_mask=attention_mask, pad_token_id=my_tokenizer.pad_token_id, max_length = ((input_ids.shape[1]) * 2) + 1)
     # Decode the generated output
-    # response_base = my_tokenizer.decode(outputs_base[0], skip_special_tokens=True)
     response_base = my_tokenizer.decode(outputs_base[0])
 
-    # Generate output from the model using both input_ids and attention_mask
+    # Generate output from lora model using both input_ids and attention_mask
     outputs_lora = lora_model.generate(input_ids=input_ids_lora, attention_mask=attention_mask_lora, pad_token_id=lora_tokenizer.pad_token_id, max_length = ((input_ids_lora.shape[1]) * 2) + 1)
     # Decode the generated output
-    # response_lora = lora_tokenizer.decode(outputs_lora[0], skip_special_tokens=True)
     response_lora = lora_tokenizer.decode(outputs_lora[0])
 
     st.write(f"Text input: {input_text}")
